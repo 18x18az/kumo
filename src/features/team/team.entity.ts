e9a5f03ef510488760b6cec11633982d44cbda69
Alec Miller <alecmichaelmiller@gmail.com>
2024-02-23 04:15:49 -0700

    additional team info

--- src/features/team/team.entity.ts
+++ src/features/team/team.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Inspection } from './team.interface'
 
 @Entity()
 export class TeamEntity {
@@ -7,4 +8,10 @@ export class TeamEntity {
 
   @Column()
     number: string
+
+  @Column({ default: 0 })
+    rank: number
+
+  @Column({ enum: Inspection, type: 'simple-enum', default: Inspection.NOT_HERE })
+    checkin: Inspection
 }
--- src/features/team/team.object.ts
+++ src/features/team/team.object.ts
@@ -1,4 +1,5 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql'
+import { Inspection } from './team.interface'
 
 @ObjectType()
 export class Team {
@@ -7,4 +8,10 @@ export class Team {
 
   @Field(() => String, { description: 'The number of the team' })
     number: string
+
+  @Field(() => Int, { description: 'The rank of the team' })
+    rank: number
+
+  @Field(() => Inspection, { description: 'The inspection status of the team' })
+    inspection: Inspection
 }
